1966e676d0225a10392594eca69c48bce9f12098
guoyao <guoyao132@163.com>
2020-04-17 13:33:37 +0800

    浮点数除法修改

--- src/plugins/common.js
+++ src/plugins/common.js
@@ -25,12 +25,15 @@ export default {
   },
   //浮点数出发
   floatDiv: function (arg1, arg2) {
+    if (arg1 == 0 || arg2 == 0) {
+      return 0;
+    }
     var t1 = 0, t2 = 0, r1, r2;
     try {t1 = arg1.toString().split(".")[1].length} catch (e) {}
     try {t2 = arg2.toString().split(".")[1].length} catch (e) {}
     r1 = Number(arg1.toString().replace(".", ""));
     r2 = Number(arg2.toString().replace(".", ""));
-    return Number((r1 / r2) * pow(10, t2 - t1));
+    return Number((r1 / r2) * Math.pow(10, t2 - t1));
   },
   //格式化日期
   /**
